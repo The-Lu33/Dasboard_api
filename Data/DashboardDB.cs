d0eff626c946fd11c4662fdf1ab8af08021609cc
The Lu <luiseducol13@gmail.com>
2023-06-12 19:34:08 -0400

    init 2

---
+++ Data/DashboardDB.cs
@@ -0,0 +1,10 @@
+using Dashboard_APi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Dashboard_APi.Data
+{
+    public class DashboardDB : DBContext
+    {
+        public DashboardDBt(DBContext])
+        public DbSet<Users> User => set<Users>();
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+namespace Dashboard_APi.Models
+{
+    public class Users
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+
+    }
+}
